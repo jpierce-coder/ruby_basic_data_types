f90c07b4eece0d3d4b19ff8528f7418eb1aec5f8
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-04-12 09:18:52 -0500

    Input & Output

---
+++ variables.rb
@@ -0,0 +1,16 @@
+# Variables in Ruby
+
+age = 18
+
+# age = age + 4 # would then equal 22
+
+# age += 4 # same as above but shorthand. Would equal 22
+# age -= 4 # 14
+# age *= 2 # 36
+# age /= 2 # 9
+
+my_location = "Missouri"
+
+johns_location = my_location # assigns my_location to johns_location making the value of both 'Missouri'
+# SNAKE CASE IS STANDARD IN RUBY
+
