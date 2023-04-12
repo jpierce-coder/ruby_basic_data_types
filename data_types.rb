a1510e6806c33b05de6d6dae1f6d8ffdb0e2875d
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-04-11 22:39:06 -0500

    Code Along - Basic Data Types
    - performed code exercises along with video

---
+++ data_types.rb
@@ -0,0 +1,15 @@
+name = "Odin"
+
+puts "Hello, #{name}!"
+
+puts 5.to_s
+
+puts nil.to_s
+
+puts :symbol.to_s
+
+puts "string" == "string"
+
+puts "string".object_id == "string".object_id
+
+puts :symbol.object_id == :symbol.object_id
